0b227549e750929cdc52de126857b2de6a0d4fe5
Rishie <rishieacc@gmail.com>
2026-04-20 14:54:16 +1000

    remove old

--- src/TeamControl/world/model_manager.py
+++
@@ -1,7 +0,0 @@
-from TeamControl.world.model import WorldModel
-from multiprocessing.managers import BaseManager
-
-class WorldModelManager(BaseManager): pass
-
-WorldModelManager.register('WorldModel', WorldModel)
-    
